818bc62190ed063396c1d0c6d476a01d6317c564
leonardo.sabino <leonardo.sabino.s@hotmail.com>
2020-08-03 21:51:14 -0300

    add save configs system

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,17 +1,24 @@
 extends Node2D
 	
 var score = 0 setget setScore
+var scoreMax = 0
 var volume = 0 setget setVolume
 
 signal score_changed
 
+func _ready():
+	load_game()
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), volume)
+	pass
+
 func _process(delta):
-	
 	# Global Score Game
 	if has_node("/root/main/game"): 
 		setScore(score + delta)
 		pass
-		
+	
+	if (scoreMax < score):
+		scoreMax = score
 	pass
 	
 func setScore(value):
@@ -23,3 +30,27 @@ func setVolume(value):
 	volume = value
 	pass
 	
+func save_game():
+	var save_game = File.new()
+	save_game.open("user://savegame.save", File.WRITE)
+	var save_dict = {
+		"scoreMax": scoreMax,
+		"volume": volume
+	}
+	save_game.store_line(to_json(save_dict))
+	save_game.close()
+
+func _exit_tree():
+	save_game()
+
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists("user://savegame.save"):
+		return 
+
+	save_game.open("user://savegame.save", File.READ)
+	var node_data = parse_json(save_game.get_line())
+	volume = node_data["volume"]
+	scoreMax = node_data["scoreMax"]
+		
+	save_game.close()
